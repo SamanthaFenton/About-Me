b6660d97891ae2ec8f14536d58a671016f8869ac
Samantha Fenton <samantha.fenton@live.co.uk>
2023-06-14 13:30:19 +0100

    JS updated on switch and if

--- app.js
+++ app.js
@@ -4,92 +4,105 @@ console.log(1)
 const welcome = alert("Welcome to my website about me, I hope you enjoy it, but first....");
 console.log(welcome)
 
-let user = prompt("Please tell me your name?");
+let user = prompt("Please tell me your name?").toLowerCase();
 console.log(user);
 
 const greet = alert("OK, thanks " + user + " now you may enter...ENJOY!!!");
-console.log(greet)
+console.log(greet);
+
+const intro = alert("Before you get started could you please try to answer a few questions about me first... please answer with `yes/no' or `y/n' only!   THANK YOOOOOOO.");
+console.log(intro);
 
 
 
+let qone = prompt("Can I make a Dolphin noise???").toLowerCase();
+console.log(qone)
 
-let q1 = prompt(Can I make a Dolphin noise???);
-console.log(q1)
-
-switch .toLowerCase(q1){
-    case "yes||y":
-    console.log("That's correct, Well done!")
-    break;
-    case "no||n":
-    console.log("XXX - Wrong answer... please try again")
-    break;
+switch(qone){
+    case "yes":
+    case "y":
+        console.log("That's correct, Well done!");
+        break;
+    case "no":
+    case "n":    
+        console.log("XXX - Wrong answer... please try again");
+        break;
     default:
-        console.log("no idea what you said!")
+        console.log("no idea what you said!");
 
 }
 
-let q2 = prompt("Do I enjoy surprises???");
-console.log(q2)
+let qtwo = prompt("Do I enjoy surprises???").toLowerCase();
+console.log(qtwo)
 
-switch .toLowerCase(q2){
-    case "yes||y":
-        console.log("ABSOLUTELY NOT, it's just not fun!!")
+switch (qtwo){
+
+    case "yes":
+    case "y":
+        console.log("ABSOLUTELY NOT, it's just not fun!!");
         break;
-    case "no||n":
-        console.log("Yep!  Totally right, surprises suck!")
-        break;
-    default:
-        console.log("no idea what you said!")
-
-}
-
-
-let q3 = prompt("Do I speak more than 1 language?")
-console.log(q3)
-
-
-switch .toLowerCase(q2){
-    case "yes||y":
-        console.log("well done, now you're getting me!")
-        break;
-    case "no||n":
-        console.log("Wronnnnggggggggg!")
+    case "no":
+    case "n":
+        console.log("Yep!  Totally right, surprises suck!");
         break;
     default:
-        console.log("That's just noise!")
+        console.log("no idea what you said!");
 
 }
 
 
-let q4 = prompt("Have I ever lived in another country???")
-console.log(q4)
+let qthree = prompt("Do I speak more than 1 language?").toLowerCase();
+console.log(qthree)
 
-switch .toLowerCase(q2){
-    case "yes||y":
-        console.log("I would have loved to live or have lived in another, but alas, destiny had another path for me.")
+
+switch (qthree){
+    case "yes":
+    case "y":
+        console.log("well done, now you're getting me!");
         break;
-    case "no||n":
-        console.log("Yeah you're right, totally missed that boat!!")
+    case "no":
+    case "n":
+        console.log("Wronnnnggggggggg!");
         break;
     default:
-        console.log("Boooooooooooooooooooooooo!")
+        console.log("That's just noise!");
+
+}
+
+
+let qfour = prompt("Have I ever lived in another country???").toLowerCase();
+console.log(qfour)
+
+switch (qfour){
+    case "yes":
+    case "y":
+        console.log("I would have loved to live or have lived in another, but alas, destiny had another path for me.");
+        break;
+    case "no":
+    case "n":
+        console.log("Yeah you're right, totally missed that boat!!");
+        break;
+    default:
+        console.log("Boooooooooooooooooooooooo!");
 
 }
 
 
 
-let q5 = confirm("Have I ever been on a TV gameshow???")
-console.log(q5)
+let qfive = confirm("Have I ever been on a TV gameshow???").toLowerCase();
+console.log(qfive)
 
-switch .toLowerCase(q2){
-    case "yes||y":
-        console.log("Absolutely NOOOO way.... never in a million years... do you even know me???")
+switch (qfive){
+    case "yes":
+    case "y":
+        console.log("Absolutely NOOOO way.... never in a million years... do you even know me???");
         break;
-    case "no||n":
-        console.log("Thaaaaaaat's right... and I never ever will!")
+    case "no":
+    case "n":
+        console.log("Thaaaaaaat's right... and I never ever will!");
         break;
     default:
-        console.log("You're just no fun!")
+        console.log("You're just no fun!");
 
 }
 
--- app1.js
+++ app1.js
@@ -13,46 +13,46 @@ console.log(greet)
 
 
 
-let q1 = confirm(Can I make a Dolphin noise ???);
+let qone = confirm(Can I make a Dolphin noise ???);
 console.log(q1)
 
-if (q1 = true) {
+if (qone = true) {
     console.log("That's correct, Well done!")
 } else {
     console.log("XXX - Wrong answer... please try again")
 }
 
-let q2 = confirm("Do I enjoy surprises???");
+let qtwo = confirm("Do I enjoy surprises???");
 console.log(q2)
 
-if (q2 = true) {
+if (qtwo = true) {
     console.log("ABSOLUTELY NOT, it's just not fun!!")
 } else {
     console.log("Yep!  Totally right, surprises suck!")
 }
 
-let q3 = confirm("Do I speak more than 1 language?")
+let qthree = confirm("Do I speak more than 1 language?")
 console.log(q3)
 
-if (q3 = true) {
+if (qthree = true) {
     console.log("well done, now you're getting me!")
 } else {
     console.log("Wronnnnggggggggg!")
 }
 
-let q4 = confirm("Have I ever lived in another country???")
+let qfour = confirm("Have I ever lived in another country???")
 console.log(q4)
 
-if (q4 = true) {
+if (qfour = true) {
     console.log("I would have loved to live or have lived in another, but alas, destiny had another path for me.")
 } else {
     console.log("Yeah you're right, totally missed that boat!")
 }
 
-let q5 = confirm("Have I ever been on a TV gameshow???")
+let qfive = confirm("Have I ever been on a TV gameshow???")
 console.log(q5)
 
-if (q5 = true) {
+if (qfive = true) {
     console.log("Absolutely NOOOO way.... never in a million years... do you even know me???")
 } else {
     console.log("Thaaaaaaat's right... and I never ever will!")
